6bf98ec57b8e016ecd4c908adeab4c2f455c26ee
C:\Lecture <https://github.com/rlaskarb/240826-.git>
2024-10-10 16:21:04 +0900

    10/10 R -1

--- chap00-remix-lecture-source/src/main/java/com/boram/section01/remix/view/PrintResultViewNamKyu.java
+++ chap00-remix-lecture-source/src/main/java/com/boram/section01/remix/view/PrintResultViewNamKyu.java
@@ -1,4 +1,13 @@
 package com.boram.section01.remix.view;
 
+import com.boram.section01.remix.model.dto.EmployeeDTONamKyu;
+
+import java.util.List;
+
 public class PrintResultViewNamKyu {
+
+
+
+
+
 }
