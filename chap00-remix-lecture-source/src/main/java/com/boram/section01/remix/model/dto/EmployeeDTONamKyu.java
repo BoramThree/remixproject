7adad473ffe13dcb30702841cee6dee241f8ad32
rlaskarb <rlaskarb20@gmail.com>
2024-10-10 23:41:36 +0900

    10/10 namkyu

--- chap00-remix-lecture-source/src/main/java/com/boram/section01/remix/model/dto/EmployeeDTONamKyu.java
+++ chap00-remix-lecture-source/src/main/java/com/boram/section01/remix/model/dto/EmployeeDTONamKyu.java
@@ -6,21 +6,9 @@ public class EmployeeDTONamKyu {
     private int empID;
     private String empName;
     private String empNo;
-    private String email;
-    private int phone;
-    private String deptCode;
-    private String jobCode;
-    private String salLevel;
     private int salary;
     private double bonus;
-    private int managerId;
-    private Date hireDate;
-    private Date entDate;
-    private String entYn;
 
-
-
-   // getter / setter
 
     public int getEmpID() {
         return empID;
@@ -46,46 +34,6 @@ public class EmployeeDTONamKyu {
         this.empNo = empNo;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getPhone() {
-        return phone;
-    }
-
-    public void setPhone(int phone) {
-        this.phone = phone;
-    }
-
-    public String getDeptCode() {
-        return deptCode;
-    }
-
-    public void setDeptCode(String deptCode) {
-        this.deptCode = deptCode;
-    }
-
-    public String getJobCode() {
-        return jobCode;
-    }
-
-    public void setJobCode(String job_Code) {
-        this.jobCode = job_Code;
-    }
-
-    public String getSalLevel() {
-        return salLevel;
-    }
-
-    public void setSalLevel(String salLevel) {
-        this.salLevel = salLevel;
-    }
-
     public int getSalary() {
         return salary;
     }
@@ -102,57 +50,15 @@ public class EmployeeDTONamKyu {
         this.bonus = bonus;
     }
 
-    public int getManagerId() {
-        return managerId;
-    }
-
-    public void setManagerId(int managerId) {
-        this.managerId = managerId;
-    }
-
-    public Date getHireDate() {
-        return hireDate;
-    }
-
-    public void setHireDate(Date hireDate) {
-        this.hireDate = hireDate;
-    }
-
-    public Date getEntDate() {
-        return entDate;
-    }
-
-    public void setEntDate(Date entDate) {
-        this.entDate = entDate;
-    }
-
-    public String getEntYn() {
-        return entYn;
-    }
-
-    public void setEntYn(String entYn) {
-        this.entYn = entYn;
-    }
-
-    //toString
 
     @Override
     public String toString() {
         return "EmployeeDTONamKyu{" +
                 "empID=" + empID +
                 ", empName='" + empName + '\'' +
-                ", empNo=" + empNo +
-                ", email='" + email + '\'' +
-                ", phone=" + phone +
-                ", deptCode='" + deptCode + '\'' +
-                ", jobCode='" + jobCode + '\'' +
-                ", salLevel='" + salLevel + '\'' +
+                ", empNo='" + empNo + '\'' +
                 ", salary=" + salary +
                 ", bonus=" + bonus +
-                ", managerId=" + managerId +
-                ", hireDate=" + hireDate +
-                ", entDate=" + entDate +
-                ", entYn='" + entYn + '\'' +
                 '}';
     }
 }
--- chap00-remix-lecture-source/src/main/java/com/boram/section01/remix/view/EmployeeViewNamKyu.java
+++ chap00-remix-lecture-source/src/main/java/com/boram/section01/remix/view/EmployeeViewNamKyu.java
@@ -18,7 +18,7 @@ public class EmployeeViewNamKyu {
     do{
         System.out.println("=====Employee 메뉴 관리 시스템====");
         System.out.println(" 1.메뉴 전체 조회하기");
-        System.out.println(" 2 아이디로 직원 조회하기 ");
+        System.out.println(" 2 보너스 있는 직원 조회하기 ");
         System.out.println(" 3 보너스 추가 등록하기 ");
         System.out.println(" 4 보너스 수정하기");
         System.out.println(" 5 보너스 삭제하기");
@@ -40,10 +40,10 @@ public class EmployeeViewNamKyu {
 
     private static Map<String,String> inputEmployeeBonus(){
         Scanner sc = new Scanner(System.in);
-        System.out.println("검색하실 보너스를 입력 해주세요!! : ");
-        String empBonus = sc.nextLine();
+        System.out.print("검색하실 회원 아이디를 입력 해주세요!! : ");
+        String empId = sc.nextLine();
         Map<String,String> parameter = new HashMap<>();
-        parameter.put("empBonus",empBonus);
+        parameter.put("empId",empId);
         return parameter;
 
     }
